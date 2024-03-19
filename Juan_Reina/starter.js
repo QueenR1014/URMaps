51e6d4398025d0fb42b2cf6b509879c2dc60084f
QueenR1014 <129343853+QueenR1014@users.noreply.github.com>
2024-03-19 12:34:40 -0500

    Update starter.js
    Have to begin creating function

--- Juan_Reina/starter.js
+++ Juan_Reina/starter.js
@@ -2,12 +2,12 @@
 
 console.log("hello world");
 
-const map_graph = [['A','B'], ['B','C'], ['C','D'], ['D','G'], ['G','F'],['F','E'], ['D','H'], ['H','I'], ['H','J']];
+const map_graph = [['A','B'],['A','E'], ['B','C'], ['C','D'], ['D','G'], ['G','F'],['F','E'], ['D','H'], ['H','I'], ['H','J']];
 
 const names = [['A', 'Entrada'], ['B', 'Escalera Santander'], ['C', 'Baños Carrasquilla'], ['D', 'Entrada Teatrino'], ['E', 'Museo Rosarista'],
             ['F', 'Aula Mutis'], ['G','La Bordadita'], ['H','Cuenteros'], ['I', 'Escaleras Casur'], ['J','Comedor']];   
 
 
-array pathfinder(){
-
+function pathfinder(destiny){
+    
 }
