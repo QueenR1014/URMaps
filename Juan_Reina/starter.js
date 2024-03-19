47de8ab1662d57a3ec8df0042b296303c9a4ffd4
QueenR1014 <129343853+QueenR1014@users.noreply.github.com>
2024-03-19 12:21:40 -0500

    Add files via upload

---
+++ Juan_Reina/starter.js
@@ -0,0 +1,13 @@
+'use strict'
+
+console.log("hello world");
+
+const map_graph = [['A','B'], ['B','C'], ['C','D'], ['D','G'], ['G','F'],['F','E'], ['D','H'], ['H','I'], ['H','J']];
+
+const names = [['A', 'Entrada'], ['B', 'Escalera Santander'], ['C', 'Baños Carrasquilla'], ['D', 'Entrada Teatrino'], ['E', 'Museo Rosarista'],
+            ['F', 'Aula Mutis'], ['G','La Bordadita'], ['H','Cuenteros'], ['I', 'Escaleras Casur'], ['J','Comedor']];   
+
+
+array pathfinder(){
+
+}
