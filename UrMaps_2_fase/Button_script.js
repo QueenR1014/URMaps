77a863bb9d231f8bc1183f36ace4e773fe406b4e
Juanes981 <juanestorrescortes@gmail.com>
2024-05-13 17:30:37 -0500

    Update Button_script.js

--- UrMaps_2_fase/Button_script.js
+++ UrMaps_2_fase/Button_script.js
@@ -1,5 +1,4 @@
-
-// Diccionario de nombres
+// Diccionario de los id´s de cada lugar 
 const id_s = [
     ['m2', 'Entrada'], ['m3', 'Escalera Caldas'], ['m4', 'Baños Carrasquilla'],
     ['m5', 'Entrada Teatrino'], ['m6', 'Síndico'], ['m7', 'Aula Mutis'],
@@ -7,39 +6,40 @@ const id_s = [
     ['m11', 'Comedor']
 ];
 
-// Define una variable para almacenar las letras
+// Asignar la letra correspondiente de 'Entrada' a una varibale que nos indica el inicio del recorrido 
 let start = 'A'
 
+// Función que nos va ayudar a extraer el valor de la letra (A - J) de los destinos
 function getLetterFromName(name) {
     for (let i = 0; i < names.length; i++) {
         if (names[i][1] === name) {
             return names[i][0];
         }
     }
-    return null; // Devuelve null si no se encuentra la letra correspondiente al nombre
+    return null; 
 }
 
-// Función para obtener el nombre asociado a un botón
+// Función que nos va ayudar a extraer el nombre asociado a los id´s registrados (m2 - m11) de los destinos
 function getNombreDelBoton(id) {
     for (let i = 0; i < id_s.length; i++) {
         if (id_s[i][0] === id) {
             return id_s[i][1];
         }
     }
-    return null; // Devuelve null si no se encuentra el nombre
+    return null; 
 }
 
-// Función para manejar el clic en los botones
+// Función que nos permitira que se guarde el camino más optimo desde el punto 'A' al destino seleccionado
 function manejarClic(id) {
     const end = getNombreDelBoton(id);
-
     if (end) {
         visualization(start, getLetterFromName(end));
     } else {
-        console.error("No se encontró el nombre del botón para el id:", id);
+        console.error(" ", id);
     }
 }
-// Obtener los botones y añadir un evento de clic a cada uno
+
+// Bucle que nos va ayduar que se pueda ejecutar el evento correspondiente al guiado del mapa
 for (let i = 2; i <= 11; i++) {
     const buttonId = 'm' + i;
     document.getElementById(buttonId).addEventListener("click", function() {
@@ -47,4 +47,4 @@ for (let i = 2; i <= 11; i++) {
     });
 }
 
-getNombreDelBoton('m2');
+
