393c4bc66729f592e4785260fb5ad787ce0ad14b
Juanes981 <juanestorrescortes@gmail.com>
2024-05-15 15:14:30 -0500

    Cambio_carrusel.2.1

--- UrMaps_2_fase/Button_script.js
+++ UrMaps_2_fase/Button_script.js
@@ -6,6 +6,10 @@ const id_s = [
     ['m11', 'Comedor']
 ];
 
+let imagesById = [
+    ["m11", "patoimg/patopa.jpg", "patoimg/patochiquito.jpg", "patoimg/Pato3.jpeg"],
+];
+
 // Asignar la letra correspondiente de 'Entrada' a una varibale que nos indica el inicio del recorrido 
 let start = 'A'
 
@@ -29,6 +33,59 @@ function getNombreDelBoton(id) {
     return null; 
 }
 
+function getImagesById(id) {
+    for (let i = 0; i < imagesById.length; i++) {
+        if (imagesById[i][0] === id) {
+            return imagesById[i].slice(1);
+        }
+    }
+    return null;
+}
+
+function mostrarCarrusel(id) {
+    const carruselContainer = document.querySelector('.slide-list');
+    carruselContainer.innerHTML = '';
+    const images = getImagesById(id);
+
+    if (images) {
+        images.forEach(imageSrc => {
+            const imageElement = document.createElement('img');
+            imageElement.src = imageSrc;
+            imageElement.alt = 'Slide';
+            const slideItem = document.createElement('div');
+            slideItem.classList.add('slide-item');
+            slideItem.appendChild(imageElement);
+            carruselContainer.appendChild(slideItem);
+        });
+    }
+
+    // Reset the currentIndex to ensure the first image is shown
+    currentIndex = 0;
+    updateSlidePosition();
+}
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.slide-item');
+    const totalSlides = slides.length;
+
+    currentIndex += direction;
+
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    }
+
+    updateSlidePosition();
+}
+
+function updateSlidePosition() {
+    const newTransform = `translateX(${-currentIndex * 100}%)`;
+    document.querySelector('.slide-list').style.transform = newTransform;
+}
+
 // Función que nos permitira que se guarde el camino más optimo desde el punto 'A' al destino seleccionado
 function manejarClic(id) {
     const end = getNombreDelBoton(id);
@@ -38,8 +95,6 @@ function manejarClic(id) {
         console.error(" ", id);
     }
 }
-
-
 
 // Bucle que nos va ayduar que se pueda ejecutar el evento correspondiente al guiado del mapa
 for (let i = 2; i <= 11; i++) {
