c53ec7dca40c10a589c9df097fd0763a08d184d6
DanielM13031 <145301431+simonvelez@users.noreply.github.com>
2024-05-21 15:41:12 -0500

    arreglo del caudro de texto y el tactil

--- UrMaps_2_fase/items_script.js
+++ UrMaps_2_fase/items_script.js
@@ -50,3 +50,45 @@ document.getElementById('switch-color').addEventListener('change', function() {
     }
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    let traducciones = [];
+
+    // Cargar el archivo JSON
+    fetch('traducciones.json')
+        .then(response => response.json())
+        .then(data => {
+            traducciones = data;
+            cambiarIdioma('es'); // Idioma por defecto
+        });
+
+    function cambiarIdioma(idioma) {
+        const idiomaMap = {
+            'es': 'Español',
+            'en': 'Inglés',
+            'fr': 'Francés'
+        };
+
+        // Actualizar el texto de los elementos traducidos
+        traducciones.forEach(item => {
+            const element = document.getElementById(item.Clave);
+            if (element) {
+                element.textContent = item[idiomaMap[idioma]];
+            }
+        });
+
+        // Actualizar el texto del botón
+        const languageButton = document.getElementById('languageButton');
+        languageButton.textContent = idioma.toUpperCase();
+    }
+
+    // Manejar el clic en los elementos del menú desplegable
+    document.querySelectorAll('.dropdown-item').forEach(item => {
+        item.addEventListener('click', event => {
+            event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+            const lang = event.target.getAttribute('data-lang');
+            cambiarIdioma(lang);
+        });
+    });
+});
+
